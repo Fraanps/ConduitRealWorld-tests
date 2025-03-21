8524f220e1576ae328a59c2b7ad1c061511848b6
Francilene Silva <psilva.francilene@gmail.com>
2025-03-21 15:38:01 +0000

    docs: configurando reports

--- cypress/e2e/editor/editor.cy.js
+++ cypress/e2e/editor/editor.cy.js
@@ -55,72 +55,72 @@ describe ('Gerenciar artigo - versão page objects', () => {
   });
 });
 
-describe.skip ("Gerenciar artigo - versão com uso de custom commands e login UI", () => {
-  beforeEach (() => {
-    cy.visit ('#/login')
-    cy.loginUI ('fran@gmail.com', '123456');
-  });
+// describe.skip ("Gerenciar artigo - versão com uso de custom commands e login UI", () => {
+//   beforeEach (() => {
+//     cy.visit ('#/login')
+//     cy.loginUI ('fran@gmail.com', '123456');
+//   });
+//
+//   it ('Criar artigo com sucesso usando tag', () => {
+//     const title = faker.lorem.sentence (3);
+//     const description = faker.lorem.sentence ();
+//     const text = faker.lorem.paragraph (2);
+//     const tag = faker.lorem.word ();
+//
+//     cy.contains ('New Article')
+//       .should ('have.attr', 'href', '#/editor')
+//       .click ();
+//
+//     cy.fillArticle (title, description, text, tag);
+//
+//     cy.intercept ('POST', 'api/articles').as ('postArticle');
+//     cy.intercept ('GET', 'api/articles/**').as ('getArticle');
+//
+//     cy.publishArticle ().then (() => {
+//       cy.wait ('@postArticle').then (({response}) => {
+//         expect (response.statusCode).to.equal (201);
+//       });
+//       cy.wait ('@getArticle').its ('response.statusCode').should ('eq', 200);
+//       cy.url ().should ('include', '/article/');
+//       cy.contains (title);
+//       cy.contains (tag);
+//     });
+//   });
+// });
 
-  it ('Criar artigo com sucesso usando tag', () => {
-    const title = faker.lorem.sentence (3);
-    const description = faker.lorem.sentence ();
-    const text = faker.lorem.paragraph (2);
-    const tag = faker.lorem.word ();
-
-    cy.contains ('New Article')
-      .should ('have.attr', 'href', '#/editor')
-      .click ();
-
-    cy.fillArticle (title, description, text, tag);
-
-    cy.intercept ('POST', 'api/articles').as ('postArticle');
-    cy.intercept ('GET', 'api/articles/**').as ('getArticle');
-
-    cy.publishArticle ().then (() => {
-      cy.wait ('@postArticle').then (({response}) => {
-        expect (response.statusCode).to.equal (201);
-      });
-      cy.wait ('@getArticle').its ('response.statusCode').should ('eq', 200);
-      cy.url ().should ('include', '/article/');
-      cy.contains (title);
-      cy.contains (tag);
-    });
-  });
-});
-
-describe.skip ("Gerenciar artigo - versão com uso de custom commands e login via API", () => {
-  beforeEach (() => {
-
-    cy.loginAPI ('fran@gmail.com', '123456').as ('session')
-    cy.visit ('#/editor')
-
-  });
-
-  it.only ('Criar artigo com sucesso usando tag', () => {
-    const title = faker.lorem.sentence (3);
-    const description = faker.lorem.sentence ();
-    const text = faker.lorem.paragraph (2);
-    const tag = faker.lorem.word ();
-
-    cy.contains ('New Article')
-      .should ('have.attr', 'href', '#/editor')
-      .click ();
-
-    cy.fillArticle (title, description, text, tag);
-
-    cy.intercept ('POST', 'api/articles').as ('postArticle');
-    cy.intercept ('GET', 'api/articles/**').as ('getArticle');
-
-    cy.publishArticle ().then (() => {
-      cy.wait ('@postArticle').then (({response}) => {
-        expect (response.statusCode).to.equal (201);
-      });
-      cy.wait ('@getArticle').its ('response.statusCode').should ('eq', 200);
-      cy.url ().should ('include', '/article/');
-      cy.contains (title);
-      cy.contains (tag);
-    });
-  });
-});
+// describe.skip ("Gerenciar artigo - versão com uso de custom commands e login via API", () => {
+//   beforeEach (() => {
+//
+//     cy.loginAPI ('fran@gmail.com', '123456').as ('session')
+//     cy.visit ('#/editor')
+//
+//   });
+//
+//   it.only ('Criar artigo com sucesso usando tag', () => {
+//     const title = faker.lorem.sentence (3);
+//     const description = faker.lorem.sentence ();
+//     const text = faker.lorem.paragraph (2);
+//     const tag = faker.lorem.word ();
+//
+//     cy.contains ('New Article')
+//       .should ('have.attr', 'href', '#/editor')
+//       .click ();
+//
+//     cy.fillArticle (title, description, text, tag);
+//
+//     cy.intercept ('POST', 'api/articles').as ('postArticle');
+//     cy.intercept ('GET', 'api/articles/**').as ('getArticle');
+//
+//     cy.publishArticle ().then (() => {
+//       cy.wait ('@postArticle').then (({response}) => {
+//         expect (response.statusCode).to.equal (201);
+//       });
+//       cy.wait ('@getArticle').its ('response.statusCode').should ('eq', 200);
+//       cy.url ().should ('include', '/article/');
+//       cy.contains (title);
+//       cy.contains (tag);
+//     });
+//   });
+// });
 
 
--- cypress_prod.config.js
+++ cypress_prod.config.js
@@ -6,10 +6,21 @@ module.exports = defineConfig ({
     passwordDefault: '123456',
     nameDefault: 'nameTest',
   },
+
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    charts: true,
+    reporterPageTitle: 'Conduit-automation-tests',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false
+  },
+
   e2e: {
     baseUrl: 'https://conduit-realworld-example-app.fly.dev/',
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 });
